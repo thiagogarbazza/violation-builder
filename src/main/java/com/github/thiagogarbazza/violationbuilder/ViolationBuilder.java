52be08be047dddd5af012e8e75862dd73e9b840f
Thiago Garbazza <thiagogarbazza@gmail.com>
2019-09-03 17:44:27 -0300

    add java doc.

--- src/main/java/com/github/thiagogarbazza/violationbuilder/ViolationBuilder.java
+++ src/main/java/com/github/thiagogarbazza/violationbuilder/ViolationBuilder.java
@@ -10,15 +10,29 @@ public class ViolationBuilder {
 
   private final SimpleMessages violations;
 
+  /**
+   * <p>Constructor for ViolationBuilder.</p>
+   * <p>Starts off assuming that no violations. Multiple calls are
+   * then made to the various append methods, followed by a call to
+   * {@link #build} to get the result.</p>
+   */
   public ViolationBuilder() {
     this.violations = new SimpleMessages();
   }
 
-  public void build() {
+  /**
+   * @throws ViolationException when there is a violation.
+   */
+  public void build() throws ViolationException {
     build(false);
   }
 
-  public void build(boolean ignoreWarnings) {
+  /**
+   * @param ignoreWarnings Condition that defines if there is any violation of warning type, they will be ignored or not.
+   *
+   * @throws ViolationException when there is a violation.
+   */
+  public void build(boolean ignoreWarnings) throws ViolationException {
     SimpleMessages thatViolations = ignoreWarnings
       ? this.violations.extractByType(ERROR)
       : this.violations;
@@ -28,10 +42,23 @@ public class ViolationBuilder {
     }
   }
 
-  public void buildIgnoreWarnings() {
+  /**
+   * @throws ViolationException when there is a violation of error type.
+   * If there is any violation of warning type, they will be ignored.
+   */
+  public void buildIgnoreWarnings() throws ViolationException {
     build(true);
   }
 
+  /**
+   * <p>Appends to the builder a violation of error type.</p>
+   *
+   * @param condition Condition that defines if the violation should be added or not.
+   * @param key Unique identifier of the violation.
+   * @param text Description of the violation.
+   *
+   * @return this - used to chain append calls
+   */
   public ViolationBuilder error(final boolean condition, final String key, final String text) {
     if (condition) {
       this.violations.add(new SimpleMessage(ERROR, key, text));
@@ -40,6 +67,15 @@ public class ViolationBuilder {
     return this;
   }
 
+  /**
+   * <p>Appends to the builder a violation of error type.</p>
+   *
+   * @param condition Condition that defines if the violation should be added or not.
+   * @param key Unique identifier of the violation.
+   * @param violationTextBuilder Lazy description builder of the violation.
+   *
+   * @return this - used to chain append calls
+   */
   public ViolationBuilder error(final boolean condition, final String key, final ViolationTextBuilder violationTextBuilder) {
     if (condition) {
       this.violations.add(new SimpleMessage(ERROR, key, violationTextBuilder.build()));
@@ -48,18 +84,43 @@ public class ViolationBuilder {
     return this;
   }
 
+  /**
+   * <p>Appends to the builder a violation of error type.</p>
+   *
+   * @param key Unique identifier of the violation.
+   * @param text Description of the violation.
+   *
+   * @return this - used to chain append calls
+   */
   public ViolationBuilder error(final String key, final String text) {
     this.violations.add(new SimpleMessage(ERROR, key, text));
 
     return this;
   }
 
+  /**
+   * <p>Appends to the builder a violation of error type.</p>
+   *
+   * @param key Unique identifier of the violation.
+   * @param violationTextBuilder Lazy description builder of the violation.
+   *
+   * @return this - used to chain append calls
+   */
   public ViolationBuilder error(final String key, final ViolationTextBuilder violationTextBuilder) {
     this.violations.add(new SimpleMessage(ERROR, key, violationTextBuilder.build()));
 
     return this;
   }
 
+  /**
+   * <p>Appends to the builder a warning of error type.</p>
+   *
+   * @param condition Condition that defines if the violation should be added or not.
+   * @param key Unique identifier of the violation.
+   * @param violationTextBuilder Lazy description builder of the violation.
+   *
+   * @return this - used to chain append calls
+   */
   public ViolationBuilder warning(final boolean condition, final String key, final ViolationTextBuilder violationTextBuilder) {
     if (condition) {
       this.violations.add(new SimpleMessage(WARNING, key, violationTextBuilder.build()));
@@ -68,6 +129,15 @@ public class ViolationBuilder {
     return this;
   }
 
+  /**
+   * <p>Appends to the builder a violation of warning type.</p>
+   *
+   * @param condition Condition that defines if the violation should be added or not.
+   * @param key Unique identifier of the violation.
+   * @param text Description of the violation.
+   *
+   * @return this - used to chain append calls
+   */
   public ViolationBuilder warning(final boolean condition, final String key, final String text) {
     if (condition) {
       this.violations.add(new SimpleMessage(WARNING, key, text));
@@ -76,18 +146,40 @@ public class ViolationBuilder {
     return this;
   }
 
+  /**
+   * <p>Appends to the builder a violation of warning type.</p>
+   *
+   * @param key Unique identifier of the violation.
+   * @param text Description of the violation.
+   *
+   * @return this - used to chain append calls
+   */
   public ViolationBuilder warning(final String key, final String text) {
     this.violations.add(new SimpleMessage(WARNING, key, text));
 
     return this;
   }
 
+  /**
+   * <p>Appends to the builder a warning of error type.</p>
+   *
+   * @param key Unique identifier of the violation.
+   * @param violationTextBuilder Lazy description builder of the violation.
+   *
+   * @return this - used to chain append calls
+   */
   public ViolationBuilder warning(final String key, final ViolationTextBuilder violationTextBuilder) {
     this.violations.add(new SimpleMessage(WARNING, key, violationTextBuilder.build()));
 
     return this;
   }
 
+  /**
+   * <p>Constructor for ViolationBuilder.</p>
+   * <p>Starts off assuming that no violations. Multiple calls are
+   * then made to the various append methods, followed by a call to
+   * {@link #build} to get the result.</p>
+   */
   public static ViolationBuilder builder() {
     return new ViolationBuilder();
   }
