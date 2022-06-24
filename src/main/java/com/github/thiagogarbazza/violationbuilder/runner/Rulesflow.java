f4c223d272f8aa4cbfc96c807133a6dc48592506
Thiago Garbazza <thiagogarbazza@gmail.com>
2022-06-24 10:27:17 -0300

    fix: error self-closing element not allowed

--- src/main/java/com/github/thiagogarbazza/violationbuilder/runner/Rulesflow.java
+++ src/main/java/com/github/thiagogarbazza/violationbuilder/runner/Rulesflow.java
@@ -10,20 +10,20 @@ public enum Rulesflow {
   /**
    * When a rule is violated, the flow of execution of rules must continue.
    * <p>
-   * <b>Example:</b><br/>
-   * ValidationRule rule02 = (violationBuilder, somebOject) -&gt; { ...; return CONTINUE;};<br/>
-   * RulesExecutor.rulesExecutor(asList(rule01, rule02, rule03), somebOject);<br/>
-   * When executing all rules, even if rule02 has a violation, <b>rule03 will be executed</b>.
+   * <b>Example:</b><br>
+   * &emsp;ValidationRule rule02 = (violationBuilder, somebOject) -&gt; { ...; return CONTINUE;};<br>
+   * &emsp;RulesExecutor.rulesExecutor(asList(rule01, rule02, rule03), somebOject);<br>
+   * &emsp;When executing all rules, even if rule02 has a violation, <i>rule03 will be executed</i>.
    */
   CONTINUE,
 
   /**
    * When a rule is violated, the flow of execution of rules must stop.
    * <p>
-   * <b>Example:</b><br/>
-   * ValidationRule rule02 = (violationBuilder, somebOject) -&gt; { ...; return STOP;};<br/>
-   * RulesExecutor.rulesExecutor(asList(rule01, rule02, rule03), somebOject);<br/>
-   * When executing all rules, even if rule02 has a violation, <b>rule03 will not be executed</b>.
+   * <b>Example:</b><br>
+   * &emsp;ValidationRule rule02 = (violationBuilder, somebOject) -&gt; { ...; return STOP;};<br>
+   * &emsp;RulesExecutor.rulesExecutor(asList(rule01, rule02, rule03), somebOject);<br>
+   * &emsp;When executing all rules, even if rule02 has a violation, <i>rule03 will not be executed</i>.
    */
   STOP
 }
